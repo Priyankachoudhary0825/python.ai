0168da57b5ddbe0609823664f804efd2999d347b
Priyankachoudhary0825 <priyankachoudhary0847@gmail.com>
2026-02-19 19:47:36 +0530

    Initial commit

---
+++ operater.py
@@ -0,0 +1,52 @@
+# 1st question 
+name=input("enter name:")
+cls=input("enter class")
+section=input("enter section:")
+r1=int(input("enter subject 1 marks:"))
+r2=int(input("enter subject 2 marks:")}
+r3=int(input("enter subject 3 marks:"))
+r4=int(input("enter subject 4 marks:"))
+r5=int(input("enter subject 5 marks:"))
+total = r1+r2+r3+r4+r5
+percentage=total/5 
+print("name:",name)
+print("class:",cls)
+print("section:",section)
+print("percentage:",percentage)   
+
+
+#2nd question
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+sum=a+b+c
+print("sum=",sum)
+
+#3rd question
+num=int(input("enter a number:"))
+square=num*num
+print("square=",square)
+
+#4th question
+celsius=input("enter temperature in celsius:")
+c=float(celsius)
+fahrenheit=(c*9/5)+32
+print("celsius:",c)
+print("fahrenheit:",fahrenheit)
+
+#5th question
+c=int(input("enter first number:"))
+d=int(input("enter second number:"))
+quotient=c//d
+remainder=c%d
+print("quotient=",quotient)
+print("remainder=",remainder)
+
+#6th question
+P=int(input("enter Principal:"))
+R=int(input("enter Rate:"))
+T=int(input("enter Time:"))
+SI=(P*R*T)/100
+print("simple interest=",SI)
+
+
